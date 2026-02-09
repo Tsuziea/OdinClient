ba9434fd007e878df9a7eb66d5d106245d4fc1bb
skies-starred <skies.starred@gmail.com>
2026-02-09 19:41:41 +0530

    feat(EtherwarpHelper)

--- src/main/java/starred/skies/odin/mixin/mixins/MouseHandlerMixin.java
+++ src/main/java/starred/skies/odin/mixin/mixins/MouseHandlerMixin.java
@@ -1,10 +1,12 @@
 package starred.skies.odin.mixin.mixins;
 
 import net.minecraft.client.MouseHandler;
+import net.minecraft.client.input.MouseButtonInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import starred.skies.odin.events.InputEvent;
 import starred.skies.odin.features.impl.cheats.FarmKeys;
 
 @Mixin(MouseHandler.class)
@@ -12,5 +14,17 @@ public class MouseHandlerMixin {
     @Inject(method = "turnPlayer", at = @At("HEAD"), cancellable = true)
     private void onTurnPlayer(CallbackInfo ci) {
         if (FarmKeys.getLock()) ci.cancel();
+    }
+
+    @Inject(method = "onButton", at = @At("HEAD"), cancellable = true)
+    private void onButton(long window, MouseButtonInfo buttonInfo, int action, CallbackInfo ci) {
+        if (action == 1) {
+            if (new InputEvent.Mouse.Press(buttonInfo).postAndCatch()) {
+                ci.cancel();
+                System.out.println("cancelled press");
+            }
+        } else if (action == 0) {
+            new InputEvent.Mouse.Release(buttonInfo).postAndCatch();
+        }
     }
 }
--- src/main/kotlin/starred/skies/odin/utils/PlayerUtils.kt
+++ src/main/kotlin/starred/skies/odin/utils/PlayerUtils.kt
@@ -1,21 +1,14 @@
 package starred.skies.odin.utils
 
 import com.odtheking.odin.OdinMod.mc
-import net.minecraft.client.KeyMapping
 import starred.skies.odin.mixin.accessors.KeyMappingAccessor
 
 fun rightClick() {
-    val key = mc.options.keyUse
-    val actualKey = (key as KeyMappingAccessor).boundKey
-    KeyMapping.set(actualKey, true)
-    KeyMapping.click(actualKey)
-    KeyMapping.set(actualKey, false)
+    val options = mc.options ?: return
+    (options.keyUse as KeyMappingAccessor).clickCount += 1
 }
 
 fun leftClick() {
-    val key = mc.options.keyAttack
-    val actualKey = (key as KeyMappingAccessor).boundKey
-    KeyMapping.set(actualKey, true)
-    KeyMapping.click(actualKey)
-    KeyMapping.set(actualKey, false)
+    val options = mc.options ?: return
+    (options.keyAttack as KeyMappingAccessor).clickCount += 1
 }
